0eb1a9f5d9fc62703f9fe4ec5088363952ca26ea
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-12-23 00:35:41 +0300

    Changes

--- app/Services/BarcodeService.php
+++ app/Services/BarcodeService.php
@@ -2,7 +2,6 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Log;
 use Milon\Barcode\Facades\DNS2DFacade as DNS2D;
 
 class BarcodeService
@@ -13,6 +12,6 @@ class BarcodeService
      */
      public function generateBarcodeQr(string $barcode): string
      {
-         return DNS2D::getBarcodePNG($barcode, 'DATAMATRIX');
+         return DNS2D::getBarcodePNGPath($barcode, 'DATAMATRIX');
      }
 }
