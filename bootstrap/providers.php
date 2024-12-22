b08f76bc93704c5b70e734588173e4b2080da9ff
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-12-23 00:42:24 +0300

    Changes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    Milon\Barcode\BarcodeServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
