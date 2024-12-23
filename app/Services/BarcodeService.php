aa56ed0f1d302ec2a2de7517d8440c75e585825b
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-12-24 02:38:46 +0300

    Changes

--- app/Services/BarcodeService.php
+++ app/Services/BarcodeService.php
@@ -12,6 +12,6 @@ class BarcodeService
      */
      public function generateBarcodeQr(string $barcode): ?string
      {
-         return 'data:image/jpeg;base64,' . DNS2D::getBarcodePNG($barcode, 'DATAMATRIX');
+         return 'data:image/jpeg;base64,' . DNS2D::getBarcodeHTML($barcode, 'DATAMATRIX');
      }
 }
