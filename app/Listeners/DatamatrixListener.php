1e75739bd6e08763d21e2841fb0238472c7af4cc
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2025-07-11 00:35:05 +0300

    Changes

--- app/Listeners/DatamatrixListener.php
+++ app/Listeners/DatamatrixListener.php
@@ -41,20 +41,21 @@ class DatamatrixListener
             $img = Image::read(base64_decode($pngBase64));
 
             $w = $img->width() * 1.5;
-            $h = $img->height() * 1.35;
-            $fontSize = 18;
-            $padding  = 10;
+            $h = $img->height() * 1.5;
+            $fontSize = 28;
+            $padding  = 5;
             $textHeight = $fontSize * 1.25;
 
             $canvas = Image::create($w, $h + $textHeight + $padding)->fill('#ffffff');
 
             $canvas->place($img, 'top', 0, 20);
 
-            $canvas->text($dm->tireName, $w/2, $h + $padding, function($font) use ($img) {
+            $canvas->text($dm->tireName, $w/2, $h - $padding, function($font) use ($img) {
                 $font->file(public_path('fonts/arial/arialmt.ttf'));
-                $font->size(20);
+                $font->size(28);
                 $font->align('center');
                 $font->valign('middle');
+                $font->lineHeight(1.45);
                 $font->wrap($img->width());
             });
 
